426c7621e6da45d5dadb44850278c83a77c36141
Ayush <ayushnigam843@gmail.com>
2024-09-09 20:57:26 +0530

    Added Single Book API

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="border-t p-5 bg-primary-950">
+            <span>@ 2024 Coders Book All Rights Reserved</span>
+        </footer>
+    )
+}
+export default Footer;
