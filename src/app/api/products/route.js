b67d73daade2b615b5f08d8b049cd2572013efd9
Ayush <ayushnigam843@gmail.com>
2024-09-14 10:21:52 +0530

    Added Database Connection

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,5 +1,11 @@
 import { NextResponse } from "next/server";
+import dbConnect from "../../lib/dbConnect";
+import User from "../../model/User";
 
 export  async function GET(req) {
-  return  NextResponse.json({"msg":"Hello MextJS API"})
+  await dbConnect();
+  const user = new User({name:"Ayush",email:"Ayush@123",password:"123"})
+  await user.save()
+  return  NextResponse.json({"msg":"Hello MextJS API" , user:user})
 }
+
